ed8a93c32c88d689762489d9b84401ccb7f7f213
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-08-05 18:20:00 +0200

    Home page ui with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import logo from './logo.svg';
-
+import styled from '@emotion/styled';
+import imgPortada from './img/imagen-criptos.png';
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+const ImagenPortada = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+const Heading = styled.h1`
+  color: white;
+  font-family: 'lato';
+  font-weight: 700;
+  margin: 100px 50px;
+  text-align: center;
+  &::after {
+    content: '';
+    width: 120px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 5px auto;
+  }
+`;
 function App() {
-  return <div>App jsx</div>;
+  return (
+    <Contenedor>
+      <ImagenPortada src={imgPortada} alt={'Imagen criptomonedas'} />
+      <div>
+        <Heading>Cotizador de Criptomonedas</Heading>
+      </div>
+    </Contenedor>
+  );
 }
 
 export default App;
