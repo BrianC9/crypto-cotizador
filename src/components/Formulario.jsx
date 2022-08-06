d3c4cf707f92af17750bab667cab966ffa93547a
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-08-06 18:57:21 +0200

    custom hook for static option fields created

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -13,6 +13,7 @@ const InputSubmit = styled.input`
   font-size: 20px;
   font-weight: 700;
   transition: background-color 0.3s ease;
+  margin-top: 30px;
   &:hover {
     cursor: pointer;
     background-color: #7a7dfe;
@@ -23,7 +24,15 @@ const handleSubmit = (e) => {
 };
 
 function Formulario() {
-  const [SelectMonedas] = useSelectMonedas('Selecciona tu moneda', monedas);
+  const [monedaSeleccionada, SelectMonedas] = useSelectMonedas(
+    'Selecciona tu moneda',
+    monedas
+  );
+  // Algo a tener en cuenta a la hora de hacer un destructuring de un array:
+  // No porque en nuestro custom hook, se llame SelectMonedas de la misma manera a la que lo hemos llamado en Formulario (aqui)
+  // significa que vamos a obtenerlo, se hace por la posición del array, porque el SelectMonedas en el customHook se encuentra en la 2da posición
+  // al igual que aquí cuando accedemos a él.
+  // Cuando hacemos un object destructuring, si que debemos hacer que coincidan los nombres
   return (
     <form onSubmit={handleSubmit}>
       <SelectMonedas />
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,24 +1,38 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 const Label = styled.label`
   color: white;
   font-family: 'Lato';
   display: block;
+  font-size: 24px;
+  font-weight: 700;
+  margin: 15px 0;
 `;
+const Select = styled.select`
+  border: none;
+  outline: none;
+  padding: 14px;
+  border-radius: 10px;
+  width: 100%;
+  font-size: 18px;
+`;
+
 function useSelectMonedas(texto, coleccionMonedas) {
+  const [state, setState] = useState('');
   const SelectMonedas = () => (
     <>
       <Label>{texto}</Label>
-      <select>
+      <Select value={state} onChange={(e) => setState(e.target.value)}>
         <option value={''}>Selecciona una moneda</option>
         {coleccionMonedas.map((moneda) => (
           <option key={moneda.id} value={moneda.id}>
             {moneda.nombre}
           </option>
         ))}
-      </select>
+      </Select>
     </>
   );
-  return [SelectMonedas];
+  return [state, SelectMonedas];
 }
 
 export default useSelectMonedas;
